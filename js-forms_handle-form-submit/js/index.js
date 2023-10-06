60e65bf55d14fc724f86f1ff045a7277bae28509
syennings <syhennings@live.com>
2023-10-06 15:46:29 +0200

    console log the elements and focus on the first name

--- js-forms_handle-form-submit/js/index.js
+++ js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,18 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  event.target.elements.firstName.focus();
+  const formData = new FormData(event.target);
+
+  //   console.log(formElements.firstName);
+  //   console.log(formElements.firstName.value);
+
+  //   console.log(formData);
+  //   firstName.focus();
+  //   event.target.elements.firstName.focus();
+});
+
+// console.log(firstName);
